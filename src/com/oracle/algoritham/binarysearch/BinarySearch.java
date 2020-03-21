f2836692ee84ce21bc25c10796f946c7b9f3ff73
Abhijith Sreenivas <abhijithsreenivas@Abhijiths-MacBook-Pro.local>
2020-03-21 21:13:54 +0530

    Changed for the negative case.

--- src/com/oracle/algoritham/binarysearch/BinarySearch.java
+++ src/com/oracle/algoritham/binarysearch/BinarySearch.java
@@ -5,17 +5,23 @@ public class BinarySearch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr1 = {2, 3, 4, 5, 6, 7};
-		System.out.println(binarySearch(arr1, 7, 0, arr1.length));
+		System.out.println(binarySearch(arr1, 16, 0, arr1.length));
 
 	}
 	
 	public static int binarySearch(int[] arr, int key, int start, int end) {
-		int mid = (start+end)/2;
-		if(start == end)
-				return -1;
-		if(arr[mid]==key)
-			return mid;
-		return (key < arr[mid]) ? binarySearch(arr,key,start,mid) : binarySearch(arr,key,mid,end);
+		
+		if(end > start) {
+			int mid = (start+end)/2;
+			if(arr[mid]==key)
+				return mid;
+			return (key < arr[mid]) ? binarySearch(arr,key,start,mid) : binarySearch(arr,key,mid+1,end);
+			
+		}else {
+			return -1;
+		}
+		
+		
 	}
 
 }
