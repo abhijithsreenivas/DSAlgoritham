1b54443ed039dc7090644a21aaa70eec2b8dbbf9
Abhijith Sreenivas <abhijithsj@hotmail.com>
2020-03-22 21:27:59 +0530

    Rotated Sorted Array First Commit.

--- src/com/oracle/algoritham/binarysearch/RotatedSortedArray.java
+++ src/com/oracle/algoritham/binarysearch/RotatedSortedArray.java
@@ -4,24 +4,59 @@ public class RotatedSortedArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr1= {4,5,6,7,1,2,3};
-		//System.out.println(getRotatedSortedArraySearch(arr1, 7, 0, arr1.length-1));
+		int[] arr1 = { 4, 5, 6, 7, 1, 2, 3 };
+		System.out.println(doRotatedSearchArray(arr1, 3));
 	}
-	
-	private static int getPivotElement(int[] arr,int start, int end) {
-		if(end>start)
+
+	private static int binarySearch(int[] arr, int key, int start, int end) {
+		if (end >= start) {
+			int mid = (start + end) / 2;
+			if (arr[mid] == key)
+				return mid;
+			return (key < arr[mid]) ? binarySearch(arr, key, start, mid) : binarySearch(arr, key, mid + 1, end);
+		} else {
 			return -1;
-		if(end==start)
-			return start;
-		int mid=(end+start)/2;
-		if(arr[mid]<arr[end] && arr[mid] > arr[mid+1])
-			return mid+1;
-		if(arr[start] > arr[mid]  && arr[mid] < arr[mid-1])
-			return mid;
-		return (arr[start] > arr[mid]) ? getPivotElement(arr, start, mid) : getPivotElement(arr, mid+1, end);
-			
+		}
+
 	}
-	
-	
+
+	private static int getPivotIndex(int[] arr, int start, int end) {
+		if (end > start) {
+			int mid = (start + end) / 2;
+			if (mid > start && mid < end) {
+				if (arr[mid] < arr[mid - 1])
+					return mid;
+				if (arr[mid + 1] < arr[mid])
+					return mid + 1;
+				return (arr[start] > arr[mid]) ? getPivotIndex(arr, start, mid) : getPivotIndex(arr, mid + 1, end);
+			}
+		}
+		return -1;
+
+	}
+
+	public static int doRotatedSearchArray(int[] arr, int key) {
+		int pivotIndex = getPivotIndex(arr, 0, arr.length - 1);
+		if (pivotIndex != -1) {
+			if (arr[pivotIndex] == key)
+				return pivotIndex;
+			if (key > arr[arr.length - 1])
+				 return binarySearch(arr, key, 0, pivotIndex);
+			else
+				return binarySearch(arr, key, pivotIndex+1, arr.length - 1);
+		}else {
+			return pivotIndex;
+		}
+		
+		
+	}
 
 }
+
+
+/** OUTPUT 
+ * 
+ * 6
+ * 
+ * **/
+
